d1dd1ee00d1c6cb0f5ddaed8ef587cf4c87fc176
trevyn <trevyn-git@protonmail.com>
2021-05-24 12:41:07 -0700

    rustfmt

--- turbocard_server/src/main.rs
+++ turbocard_server/src/main.rs
@@ -2,18 +2,15 @@ use std::io::Write;
 
 #[tokio::main]
 async fn main() {
-    eprintln!("HULLO");
+ eprintln!("HULLO");
 
-    let client = reqwest::Client::new();
-    let mut res = client
-        .post("https://api.openai.com/v1/engines/davinci/completions")
-        .header(reqwest::header::CONTENT_TYPE, "application/json")
-        .header(
-            reqwest::header::AUTHORIZATION,
-            include_str!("../../credentials/openai"),
-        )
-        .body(
-            r#"{
+ let client = reqwest::Client::new();
+ let mut res = client
+  .post("https://api.openai.com/v1/engines/davinci/completions")
+  .header(reqwest::header::CONTENT_TYPE, "application/json")
+  .header(reqwest::header::AUTHORIZATION, include_str!("../../credentials/openai"))
+  .body(
+   r#"{
   "prompt": "hello",
   "temperature": 0.7,
   "max_tokens": 64,
@@ -22,25 +19,25 @@ async fn main() {
   "presence_penalty": 0.1,
   "stream": true
 }"#,
-        )
-        .send()
-        .await
-        .unwrap();
+  )
+  .send()
+  .await
+  .unwrap();
 
-    while let Some(chunk) = res.chunk().await.unwrap() {
-        let str = String::from_utf8(chunk.to_vec()).unwrap();
-        if str == "data: [DONE]\n\n" {
-            break;
-        }
-        let data = str.trim_start_matches("data: ").trim_end_matches('\n');
-        let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+ while let Some(chunk) = res.chunk().await.unwrap() {
+  let str = String::from_utf8(chunk.to_vec()).unwrap();
+  if str == "data: [DONE]\n\n" {
+   break;
+  }
+  let data = str.trim_start_matches("data: ").trim_end_matches('\n');
+  let v: serde_json::Value = serde_json::from_str(&data).unwrap();
 
-        let s = match v["choices"][0]["text"].clone() {
-            serde_json::Value::String(s) => s,
-            _ => panic!("Aaaa"),
-        };
-        print!("{}", s);
-        std::io::stdout().flush().unwrap();
-    }
-    println!();
+  let s = match v["choices"][0]["text"].clone() {
+   serde_json::Value::String(s) => s,
+   _ => panic!("Aaaa"),
+  };
+  print!("{}", s);
+  std::io::stdout().flush().unwrap();
+ }
+ println!();
 }
